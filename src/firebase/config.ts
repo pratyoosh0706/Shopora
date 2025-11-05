fd6b164b8e8bd04995d06285bf0aa6e2d8594beb
Pratyoosh Shukla <pratyooshshukla3@gmail.com>
2025-11-05 15:22:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1058045100-e0e9b",
+  "appId": "1:731779026535:web:142a9576f30b591be14ec2",
+  "apiKey": "AIzaSyDjnKXN1hhUz0yDHUKWMvQsJAkOhpl3hys",
+  "authDomain": "studio-1058045100-e0e9b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "731779026535"
+};
